e8d290f092b3bb55a0719741bdd8dd116fbb6933
CarolinaCedro <yotechgirl@gmail.com>
2022-09-23 10:55:33 -0300

    feat: first commit

---
+++ src/test/java/io/github/CarolinaCedro/algalogapi/AlgalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.CarolinaCedro.algalogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
